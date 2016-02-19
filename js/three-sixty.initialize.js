82e1363869b29c3e55792b56179f97bbb5e9d801
Jayson Exel <contact@jayexel.com>
2016-02-19 18:09:38 -0500

    Update Pointers
    Added for in loop for pointer options array.

--- js/three-sixty.initialize.js
+++ js/three-sixty.initialize.js
@@ -1,4 +1,41 @@
 $(function () {
 	j$ = jQuery.noConflict();
-    j$('.three-sixty').threeSixty({pointers: {addPointers: 'true'}});
+    j$('.three-sixty').threeSixty({pointers: {
+		addPointers: 'true', 
+		pointer1:{
+				top:	'10px',
+				left:	'80px',
+			},
+		pointer2:	{
+				top:	'70px',
+				left:	'168px',
+		},
+		pointer3:	{
+				top:	'330px',
+				left:	'68px',
+		},
+		pointer4:	{
+				top:	'10px',
+				left:	'135px',
+		},
+		pointer5:	{
+				top:	'150px',
+				left:	'175px',
+		},
+		pointer6:	{
+				top:	'130px',
+				left:	'95px',
+		},
+		pointer7:	{
+				top:	'330px',
+				left:	'215px',
+		},
+		pointer8:	{
+				top:	'150px',
+				left:	'180px',
+		},
+		pointer9:	{
+				top:	'150px',
+				left:	'85px',
+		}}});
 });
--- js/three-sixty.js
+++ js/three-sixty.js
@@ -6,35 +6,11 @@ j$ = jQuery.noConflict();
 
     var defaults = {
         background: 'transparent',
-		pointers: {
-			addPointers: 'false',
+		addPointers: 'false',
+		pointers: {	
 			background: '#3DADFF',
 			width: '25px',
 			height: '25px',
-			pointerZeroOne:	{
-				top:	'0px',
-				left:	'75px',
-			},
-			pointerZeroTwo:	{
-				top:	'310px',
-				left:	'170px',
-			},
-			pointerZeroThree:	{
-				top: 	'50px',
-				left: 	'80px'
-			},
-			pointerOneOne:	{
-				top:	'0px',
-				left:	'130px',
-			},
-			pointerOneTwo:	{
-				top:	'310px',
-				left:	'205px',
-			},
-			pointerOneThree:	{
-				top: 	'50px',
-				left: 	'90px'
-			}
 		}
     };
 	
@@ -69,7 +45,6 @@ j$ = jQuery.noConflict();
 			pDivID++;
 		});
 		
-		pliDivID = $(".pointer>a").length
 		
 		j$('.three-sixty:first').css({'display':'block'});
 		pCir.css({'display':'none'});
@@ -111,20 +86,18 @@ function pointers(x){
 	if(x.pointers.addPointers === 'true'){
 		pCir.css({background: x.pointers.background});
 		pCir.css({width: x.pointers.width});
-		pCir.css({height: x.pointers.height});	
-		j$('.pointer-circle:eq(0)').css({top: x.pointers.pointerZeroOne.top});
-		j$('.pointer-circle:eq(0)').css({left: x.pointers.pointerZeroOne.left});
-		j$('.pointer-circle:eq(1)').css({top: x.pointers.pointerZeroTwo.top});
-		j$('.pointer-circle:eq(1)').css({left: x.pointers.pointerZeroTwo.left});
-		j$('.pointer-circle:eq(2)').css({top: x.pointers.pointerZeroThree.top});
-		j$('.pointer-circle:eq(2)').css({left: x.pointers.pointerZeroThree.left});
-		j$('.pointer-circle:eq(3)').css({top: x.pointers.pointerOneOne.top});
-		j$('.pointer-circle:eq(3)').css({left: x.pointers.pointerOneOne.left});
-		j$('.pointer-circle:eq(4)').css({top: x.pointers.pointerOneTwo.top});
-		j$('.pointer-circle:eq(4)').css({left: x.pointers.pointerOneTwo.left});
-		j$('.pointer-circle:eq(5)').css({top: x.pointers.pointerOneThree.top});
-		j$('.pointer-circle:eq(5)').css({left: x.pointers.pointerOneThree.left});
+		pCir.css({height: x.pointers.height});
 		
+		var i = 0;				
+		var counter = -1;
+		
+		for (i in options.pointers) {
+			j$('.pointer-circle:eq(' + counter + ')').css({
+				top : options.pointers[i].top,
+				left : options.pointers[i].left
+			});
+			counter++; 
+		}
 	}
 };
 
